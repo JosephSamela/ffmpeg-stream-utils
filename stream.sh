04e5a8055c95695e91c560cf165e1db3147642c2
Joseph Samela <josephsamela@gmail.com>
2018-12-30 19:35:12 -0500

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,8 +1,6 @@
 
 # https://wiki.archlinux.org/index.php/Streaming_to_twitch.tv
 
-KEY="your stream key here!"
-
 INRES="1608x1050" # input resolution
 OUTRES="1680x1050" # output resolution
 FPS="24" # target FPS
@@ -12,7 +10,7 @@ THREADS="2" # max 6
 CBR="1000k" # constant bitrate (should be between 1000k - 3000k)
 QUALITY="ultrafast"  # one of the many FFMPEG preset
 AUDIO_RATE="44100"
-STREAM_KEY="live_56801934_DeUOxpcNfBA77TETwQVJPsCpIc9b0y"
+STREAM_KEY="your_stream_key_here"
 SERVER="live-jfk" # twitch server in frankfurt, see http://bashtech.net/twitch/ingest.php for list
 
 ffmpeg -f x11grab -s "$INRES" -r "$FPS" -i :1.0 -f alsa -i pulse -f flv -ac 2 -ar $AUDIO_RATE \
