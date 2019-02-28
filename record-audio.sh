7dcf745a91c3660f5b2c3a96c2a3cdff80b7a4ba
Joseph Samela <josephsamela@gmail.com>
2019-02-27 19:16:12 -0500

    Update record-audio.sh

--- record-audio.sh
+++ record-audio.sh
@@ -1,1 +1,6 @@
-ffmpeg -f alsa -i hw:2 out.wav
+echo "\nRECORDING FROM:  `arecord -l | grep \"card 2\"`"
+FILENAME=`date '+%Y.%m.%d_%H.%M.%S.wav'`
+FILEPATH=`pwd`
+echo "RECORDING TO  :  \"$FILEPATH$FILENAME\""
+echo "Press \"q\" to stop recording\n"
+ffmpeg -hide_banner  -f alsa -i hw:2,0 -af acompressor=threshold=-21dB:ratio=9:attack=200:release=1000 $FILENAME 
