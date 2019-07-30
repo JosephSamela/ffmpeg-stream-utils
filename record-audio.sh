db7c78807c50cf8ca9841d270e9c40ccea0b631f
Joseph Samela <josephsamela@gmail.com>
2019-07-29 22:02:41 -0400

    improve formatting

--- record-audio.sh
+++ record-audio.sh
@@ -1,6 +1,10 @@
-echo "\nRECORDING FROM:  `arecord -l | grep \"card 2\"`"
+echo "RECORD AUDIO"
+echo ""
+echo "MICROPHONE :  `arecord -l | grep \"card 2\"`"
 FILENAME=`date '+%Y.%m.%d_%H.%M.%S.wav'`
 FILEPATH=`pwd`
-echo "RECORDING TO  :  \"$FILEPATH$FILENAME\""
-echo "Press \"q\" to stop recording\n"
+echo "RECORDING  :  $FILEPATH/$FILENAME"
+echo ""
+echo "Press \"q\" to stop recording"
+echo ""
 ffmpeg -hide_banner  -f alsa -i hw:2,0 -af acompressor=threshold=-21dB:ratio=9:attack=200:release=1000 $FILENAME 
