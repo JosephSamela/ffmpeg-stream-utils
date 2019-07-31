cfa25baa76e3f9fd92c95cf5b133d06eea7041f4
Joseph Samela <josephsamela@gmail.com>
2019-07-30 20:12:05 -0400

    update resolution and framerate

--- record-webcam.sh
+++ record-webcam.sh
@@ -1,3 +1,3 @@
 
 # Used ffmpeg to record webcam (/dev/video0) to file
-ffmpeg -f v4l2 -framerate 24 -video_size 1280x720 -i /dev/video0 output.mkv
+ffmpeg -f v4l2 -framerate 30 -video_size 1920x1080 -i /dev/video0 output.mkv
